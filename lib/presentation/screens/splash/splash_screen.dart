7f1350a831f5c11baeffdfc0b3924065b4fa3d40
Shakil-Shak <www.shakil.sheikh222@gmail.com>
2024-10-06 15:52:56 +0600

    splash screen add

--- lib/presentation/screens/splash/splash_screen.dart
+++ lib/presentation/screens/splash/splash_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:ootms/presentation/components/common_text.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:ootms/core/constants/color/app_color.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
@@ -7,7 +8,24 @@ class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: commonText("OOTMS", size: 20, isBold: true)),
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(50.0),
+            child: Image.asset("assets/images/logo.png"),
+          ),
+          const SizedBox(
+            height: 40,
+          ),
+          const SpinKitRing(
+            color: AppColor.primaryColor,
+            size: 70.0,
+            lineWidth: 8.0,
+          ),
+        ],
+      )),
     );
   }
 }
