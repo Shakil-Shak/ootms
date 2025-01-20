8150662f80ccdccf3d59221b59b94fd67b4bf372
siamprodhan <siambci@gmail.com>
2025-01-20 21:40:46 +0600

    live location tracking ongoing

--- lib/presentation/api/controllers/mapControllers/google_map_controller.dart
+++ lib/presentation/api/controllers/mapControllers/google_map_controller.dart
@@ -317,6 +317,8 @@ class CustomMapController extends GetxController {
         'https://maps.googleapis.com/maps/api/directions/json?origin=${origin.latitude},${origin.longitude}&destination=${destination.latitude},${destination.longitude}&key=$apiKey'));
 
     log("${response.statusCode}");
+    log("Response ========>>>> ${response.body}");
+
     if (response.statusCode == 200) {
       final data = jsonDecode(response.body);
 
