0e2fc5d54736a5f5b27f3db640df9e4bf2806c99
Shakil-Shak <shakilshak.bdcalling@gmail.com>
2025-01-21 15:24:05 +0600

    marge

--- lib/presentation/api/controllers/common/signup_otp_controller.dart
+++ lib/presentation/api/controllers/common/signup_otp_controller.dart
@@ -1,10 +1,7 @@
 import 'dart:async';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
-import 'package:ootms/helpers/prefs_helper.dart';
-import 'package:ootms/presentation/api/models/otp_model.dart';
 import 'package:ootms/presentation/api/service/api_services.dart';
-import 'package:ootms/presentation/api/sharePrefarences/local_storage_save.dart';
 import 'package:ootms/presentation/api/url_paths.dart';
 import 'package:ootms/presentation/components/common_snackbar.dart';
 
@@ -75,6 +72,7 @@ class SignUpOtpController extends ChangeNotifier {
       _isLoading = true;
       notifyListeners();
 
+      // Collect and validate OTP
       String otp =
           controllers.map((controller) => controller.text.trim()).join();
       if (otp.length < 4) {
@@ -82,11 +80,13 @@ class SignUpOtpController extends ChangeNotifier {
         return null;
       }
 
+      // Debugging logs
       print("OTP: $otp");
       if (email != null) {
         print("Email: $email");
       }
 
+      // Construct the request body
       final Map<String, String> requestBody = {
         "otp": otp,
         // "email": email.toString(),
@@ -98,17 +98,6 @@ class SignUpOtpController extends ChangeNotifier {
       print("token: $token");
       final response =
           await ApiService().otherPostRequest(url, requestBody, token: token);
-
-      if (response != null && response['status'] == "OK") {
-        final signInModel = OtpModels.fromJson(response);
-        // PrefsHelper.setString("userRole", signInModel.data.attributes.id);
-
-        saveUserAcessDetails(signInModel.data.forgetPasswordToken, "");
-
-        _isLoading = false;
-        notifyListeners();
-        return signInModel;
-      }
       print("======================================otpResponse${response}");
       print(
           "======================================otpResponse${response["statusCode"]}");
